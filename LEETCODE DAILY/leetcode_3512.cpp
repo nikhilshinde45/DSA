4d5a86ecd413cb85837c9466aebc11de1231120f
student nikhildemo <nikhilshinde2257@gmail.com>
2025-11-29 14:16:48 +0530

    POTD

---
+++ LEETCODE DAILY/leetcode_3512.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+         int n=nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        int cnt=0;
+        while(sum%k!=0){
+            cnt++;
+            sum--;
+        }
+        return cnt;
+        
+    }
+};
